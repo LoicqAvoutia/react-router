3168e70e284e42226897ab4ed5494670e787f4e6
LoicqAvoutia <avoutialoicqofficial@gmail.com>
2026-08-17 12:42:48 +0200

    aggiunta di navigazione programmatica che a seconda della navbar cerca i nomi dei prodotti e se non esistono li rifà vedere tutti

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,16 +1,33 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './products.css';
 import { Link, useNavigate, useParams } from "react-router";
 
 const products = props =>{
+    const [search, setSearch] = useState("");
     console.log(props.prodotti);
     const navigate = useNavigate();
     const { id } = useParams();
-
+    const filteredProducts = props.prodotti.filter((product) =>
+        product.title.toLowerCase().startsWith(search.toLowerCase())
+    );
+    const productsToShow =
+    filteredProducts.length === 0
+    ? props.prodotti
+    : filteredProducts;
+    
+    /*
+    
+    */
     
     return (
         <>
-        {props.prodotti.map(prodotto=>{
+        <h1>cerca prodotto</h1>
+        <input
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Cerca prodotto..."
+        />
+        {productsToShow.map(prodotto=>{
             return (
             <div key={prodotto.id} className="card">
                 <img src={prodotto.image} alt={prodotto.title} />
@@ -18,6 +35,7 @@ const products = props =>{
                 <Link to={String(prodotto.id)}>pagina prodotto</Link>
             </div>
         )})}
+        {console.log(filteredProducts)}
         </>
     )
 }
